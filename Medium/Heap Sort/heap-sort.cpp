4c0cc5502577d8b12bd728bfcbcb8c34b09ac2d8
Aarti Rai <aartijbm@gmail.com>
2023-11-06 13:31:13 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Heap Sort/heap-sort.cpp
@@ -0,0 +1,86 @@
+//{ Driver Code Starts
+// C++ program for implementation of Heap Sort
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// The functions should be written in a way that array become sorted 
+// in increasing order when heapSort() is called.
+
+class Solution {
+    // Function to build a Max Heap from an array.
+    void buildHeap(int arr[], int n) {
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+    }
+
+    // Heapify function to maintain the Max Heap property.
+    void heapify(int arr[], int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && arr[left] > arr[largest]) {
+            largest = left;
+        }
+
+        if (right < n && arr[right] > arr[largest]) {
+            largest = right;
+        }
+
+        if (largest != i) {
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+            heapify(arr, n, largest);
+        }
+    }
+
+    // Function to perform Heap Sort on an array.
+    public:
+    void heapSort(int arr[], int n) {
+        buildHeap(arr, n);
+
+        for (int i = n - 1; i > 0; i--) {
+            // Swap the root element (maximum) with the last element in the heap.
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            // After swapping, heapify the reduced heap to maintain the Max Heap property.
+            heapify(arr, i, 0);
+        }
+    }
+};
+
+
+//{ Driver Code Starts.
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+// Driver program to test above functions
+int main()
+{
+    int arr[1000000],n,T,i;
+    scanf("%d",&T);
+    while(T--){
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+    Solution ob;
+    ob.heapSort(arr, n);
+    printArray(arr, n);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
